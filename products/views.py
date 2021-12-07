3bc8137a9dba68c26b65e133be428b2699b2d038
flisanp <ida@ofstories.se>
2021-12-07 14:32:16 +0000

    added sorting and product counts to products

--- products/views.py
+++ products/views.py
@@ -5,10 +5,8 @@ from .models import Product, Category
 
 # Create your views here.
 
-
-
 def all_products(request):
-    """ A view to show all products, including sorting and search queries"""
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -23,21 +21,19 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-    if request.GET:
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -50,19 +46,22 @@ def all_products(request):
     current_sorting = f'{sort}_{direction}'
 
     context = {
-        'products': products, 
+        'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
+
     return render(request, 'products/products.html', context)
 
+
 def product_detail(request, product_id):
-    """ A view to show individual product details"""
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'product': product, 
+        'product': product,
     }
-    return render(request, 'products/product_detail.html', context) 
+
+    return render(request, 'products/product_detail.html', context)
