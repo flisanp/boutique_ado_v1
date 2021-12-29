6be057d7d2727935f36189e3033df7c12b30fbe8
flisanp <ida@ofstories.se>
2021-12-29 12:16:35 +0000

    product admin - product form

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,7 +3,6 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
-
 from .models import Order, OrderLineItem
 from products.models import Product
 from profiles.models import UserProfile
@@ -11,15 +10,14 @@ from profiles.models import UserProfile
 import json
 import time
 
-
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
 
-        def _send_confirmation_email(self, order):
-           """Send the user a confirmation email"""
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
         cust_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
@@ -27,13 +25,13 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-
+        
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )
+        )        
 
     def handle_event(self, event):
         """
