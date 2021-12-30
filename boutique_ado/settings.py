2997c291adee5243cf89b91c66f67c7a229d1fba
flisanp <ida@ofstories.se>
2021-12-30 12:52:55 +0000

    deployment part2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['idam-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -107,6 +107,11 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
